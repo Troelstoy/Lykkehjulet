eb0a23f68104b40d86a7008d496c3fc417b58266
Troels <troelstoy@outlook.com>
2021-11-27 00:56:34 +0100

    Opdaterede så der ikke er så mange knapper

--- app/src/main/java/com/example/lykkehjulet/GameFragment.kt
+++ app/src/main/java/com/example/lykkehjulet/GameFragment.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.lykkehjulet.databinding.GamefragmentBinding
 import com.example.lykkehjulet.model.ViewModel
 import com.google.android.material.snackbar.Snackbar
+import java.lang.Boolean.FALSE
+import java.lang.Boolean.TRUE
 
 //TODO: Skal vise eller holde styr på hvilke bogstaver der er gættet på
 //TODO: Skal have selve lykkehjul elementet med
@@ -18,11 +21,12 @@ import com.google.android.material.snackbar.Snackbar
 //TODO: Tilføj logik der checker om input er validt
 //TODO: Kan ikke navigere til higscores skærmen
 //TODO: Vis en warning når der ikke bliver givet rigtigt input i gæt
-//TODO: Man vinder ikke hvis man gætter alle bogstaverne
 /**
  * A [Fragment] subclass as the default destination in the navigation.
  */
 class GameFragment : Fragment() {
+
+    private var _isSpinningTime = FALSE
 
     private lateinit var binding: GamefragmentBinding
     private val viewModel: ViewModel by viewModels()
@@ -43,24 +47,47 @@ class GameFragment : Fragment() {
         binding.buttonFirst.setOnClickListener {
             val word : String = binding.textField.text.toString()
 
-            //findNavController().navigate(R.id.action_FirstFragment_to_highScores)
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
 
+            //TODO: Change back
+            /*
             if(viewModel.isUserWordCorrect(word)){
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
             }
             setVisibilitySpin()
             update()
+             */
         }
 
         binding.buttonSecond.setOnClickListener {
-            val word : String = binding.textField.text.toString()
-            if(word.isNotEmpty() && word.length == 1){
 
-            val letter : Char = word.first()
-                println(viewModel.isUserLetterCorrect(letter))
+            if(_isSpinningTime){
+                binding.buttonSecond.text = "Gæt Bogstav"
+
+                val snack = Snackbar.make(view, viewModel.spinWheel(), Snackbar.LENGTH_LONG)
+                snack.show()
+                update()
+                _isSpinningTime = FALSE
+                update()
+
+
+                println("DU har lige spunnet hjulet")
+                println(_isSpinningTime)
+
+
+            }else {
+                binding.buttonSecond.text = "Spin the wheel"
+                val word: String = binding.textField.text.toString()
+
+                if (word.isNotEmpty() && word.length == 1) {
+
+                    val letter: Char = word.first()
+                    println(viewModel.isUserLetterCorrect(letter))
+
+                }
+                    _isSpinningTime = TRUE
+                update()
             }
-            update()
-            setVisibilitySpin()
 
 
         }
@@ -73,8 +100,6 @@ class GameFragment : Fragment() {
         binding.currentword.text = viewModel.guessWord
 
 
-
-
         /**
          * Dummy måde at indsætte et tal på
          */
@@ -82,25 +107,17 @@ class GameFragment : Fragment() {
         ("Score: " + viewModel.score.value.toString()).also { binding.score.text = it }
 
 
-
         /**
          * Button for spinning the wheel
          */
         binding.spinwheel.setOnClickListener {
 
-        val snack = Snackbar.make(view, viewModel.spinWheel(), Snackbar.LENGTH_LONG)
-            snack.show()
-            setVisibilityGuess()
-            update()
 
 
 
 
         }
-
-        setVisibilitySpin()
-
-
+        update()
     }
 
     private fun update(){
@@ -108,18 +125,17 @@ class GameFragment : Fragment() {
         ("Score: " + viewModel.score.value.toString()).also { binding.score.text = it }
         binding.currentword.text = viewModel.guessWord
         isGameOver()
-    }
 
-    private fun setVisibilitySpin(){
-        binding.buttonFirst.visibility = View.INVISIBLE
-        binding.buttonSecond.visibility = View.INVISIBLE
-        binding.spinwheel.visibility = View.VISIBLE
-    }
 
-    private fun setVisibilityGuess(){
-        binding.buttonFirst.visibility = View.VISIBLE
-        binding.buttonSecond.visibility = View.VISIBLE
-        binding.spinwheel.visibility = View.INVISIBLE
+        if (_isSpinningTime){
+            binding.buttonFirst.visibility = View.INVISIBLE
+            binding.buttonSecond.visibility = View.VISIBLE
+            binding.spinwheel.visibility = View.INVISIBLE
+        }else {
+            binding.buttonFirst.visibility = View.VISIBLE
+            binding.buttonSecond.visibility = View.VISIBLE
+            binding.spinwheel.visibility = View.INVISIBLE
+        }
     }
 
     //TODO: Implement
